d8b59e7b0873ea653f6938506246ead8c9d3b547
Ray Deck <ray@raydeck.com>
2020-06-24 14:54:40 -0500

    Cleaning up event listeners

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { RNSRegistry, RNKRegistryModule } = NativeModules;
 //#region event management
 let emitter;
 let subscriptions = {};
-if (Platform.OS === "ios") emitter = NativeEventEmitter(RNSRegistry);
+if (Platform.OS === "ios") emitter = new NativeEventEmitter(RNSRegistry);
 let listener = null;
 const updateSubscriptions = () => {
   if (!emitter) return;
@@ -11,13 +11,10 @@ const updateSubscriptions = () => {
     listener.remove();
     listener = null;
   } else if (!listener) {
-    listener = emitter.addEventListener(
-      "RNSRegistry",
-      ({ type, key, data }) => {
-        const compoundKey = [type, key].join("_");
-        if (subscriptions[compoundKey]) subscriptions[compoundKey](data);
-      }
-    );
+    listener = emitter.addListener("RNSRegistry", ({ type, key, data }) => {
+      const compoundKey = [type, key].join("_");
+      if (subscriptions[compoundKey]) subscriptions[compoundKey](data);
+    });
   }
   if (emitter)
     emitter.addEventListener("RNSRegistry", ({ type, key, data }) => {
@@ -30,38 +27,37 @@ const updateSubscriptions = () => {
 const setData = async (key, data) => {
   if (Platform.OS === "android")
     return await RNKRegistryModule.setData(key, data);
-  return await NativeRNSRegistry.setData(key, data);
+  return await RNSRegistry.setData(key, data);
 };
 const saveData = async (key, data) => {
   if (Platform.OS === "android") throw "Not supported on android";
-  return await NativeRNSRegistry.saveData(key, data);
+  return await RNSRegistry.saveData(key, data);
 };
 const removeData = async (key) => {
   if (Platform.OS === "android") return await RNKRegistryModule.removeData(key);
 
-  return await NativeRNSRegistry.removeData(key);
+  return await RNSRegistry.removeData(key);
 };
 const getData = async (key) => {
   if (Platform.OS === "android") return await RNKRegistryModule.getData(key);
-  return await NativeRNSRegistry.getData(key);
+  return await RNSRegistry.getData(key);
 };
 const addEvent = async (type, key = "RNAdvanced", callback) => {
   if (Platform.OS === "android")
     return await RNKRegistryModule.addEvent(type, key);
   const compoundKey = [type, key].join("_");
-  if (subcriptions[compoundKey]) subscriptions[compoundKey].remove();
-  await NativeRNSRegistry.addEvent(type, key);
+  if (subscriptions[compoundKey]) subscriptions[compoundKey].remove();
+  await RNSRegistry.addEvent(type, key);
   subscriptions[compoundKey] = callback;
   updateSubscriptions();
 };
 const removeEvent = async (type, key = "RNAdvanced") => {
-  if (Platform.OS === "android")
-    RNKRegistryModule.removeEvent(key, label, callback);
+  if (Platform.OS === "android") RNKRegistryModule.removeEvent(key, label);
   const compoundKey = [type, key].join("_");
   if (subscriptions(compoundKey)) subscriptions[compoundKey].remove();
   delete subscriptions[compoundKey];
   updateSubscriptions();
-  return await NativeRNSRegistry.removeEvent(key);
+  return await RNSRegistry.removeEvent(type, key);
 };
 //#endregion
 //#region Exports
