48642f648bb7988b7395b1c339ab66c7c84db05e
Ray Deck <ray@raydeck.com>
2020-06-24 09:00:57 -0500

    Make all events and data keys case-insensitive

--- ios/RNSRegistry/RNSMainRegistry.swift
+++ ios/RNSRegistry/RNSMainRegistry.swift
@@ -2,77 +2,95 @@ import Foundation
 public typealias cbtype = (Any) -> Bool
 var events:[String: [String: cbtype]] = [:]
 var data:[String: Any]?
-var q = DispatchQueue(label: "RNSQueue")
+var q = DispatchQueue(label: "RNSQueueEvents")
+var dq = DispatchQueue(label: "RNSQueueData")
 var savedData:[String: Any]?
 public protocol RNSStartable {
     static func runOnStart(_ application:UIApplication)->Void
 }
 open class RNSMainRegistry {
     public class func addEvent(type: String, key: String, callback: @escaping cbtype) -> String {
+        let newtype = type.lowercased()
         q.sync() {
-            if events[type] == nil { events[type] = [:] }
-            events[type]?[key] = callback
+            if events[newtype] == nil { events[newtype] = [:] }
+            events[newtype]?[key] = callback
         }
         return key
     }
     public class func removeEvent(type: String, key: String) {
+        let newtype = type.lowercased()
         let _ = q.sync() {
-            events[type]?.removeValue(forKey: key)
+            events[newtype]?.removeValue(forKey: key)
         }
     }
     public class func removeEvent(key: String) {
+        let newtype = key.lowercased()
         let _ = q.sync() {
             events.keys.forEach() { k in
-                events[k]?.removeValue(forKey: key)
+                events[k]?.removeValue(forKey: newtype)
             }
         }
     }
     public class func removeEvents(type: String) {
+        let newtype = type.lowercased()
         let _ = q.sync() {
-            guard let e = events[type] else { return }
+            guard let e = events[newtype] else { return }
             e.keys.forEach() { k in
-                events[type]?.removeValue(forKey: k)
+                events[newtype]?.removeValue(forKey: k)
             }
             events[type] = nil
         }
     }
+    public class func triggerEventNoSync(type: String, data: Any) ->Bool {
+        let newtype = type.lowercased()
+        guard let es = events[newtype] else { return false }
+        for thisKey:String in es.keys {
+            guard let cb = es[thisKey] else { continue }
+            if !cb(data) { return false }
+        }
+        return true
+    }
     public class func triggerEvent(type: String, data: Any) -> Bool {
+//        if(DispatchQueue.getSpecific(key:"label") === q.label) {
+//            triggerEventNoSync(type: type, data: data)
+//        } else {
+        let newtype = type.lowercased()
         return q.sync() {
-            guard let es = events[type] else { return false }
-            for thisKey:String in es.keys {
-                guard let cb = es[thisKey] else { continue }
-                if !cb(data) { return false }
-            }
-            return true
+            triggerEventNoSync(type: newtype, data: data)
         }
     }
     public class func setData(key: String, value: Any) {
-        let _ = q.sync() {
+        let newkey = key.lowercased()
+        let _ = dq.sync() {
             if(data == nil) { data = loadData()}
-            data![key] = value
+            data![newkey] = value
         }
     }
     public class func getData(key: String) -> Any? {
-        // return q.sync() {
+         let newkey = key.lowercased()
+         return dq.sync() {
             if(data == nil) { data = loadData()}
-            return data![key]
-        // }
+            return data![newkey]
+         }
     }
     public class func removeData(key: String) {
-        let _ = q.sync() {
+        let newkey = key.lowercased()
+        let _ = dq.sync() {
             if(data == nil) { data = loadData()}
-            data!.removeValue(forKey: key)
+            data!.removeValue(forKey: newkey)
         }
     }
     public class func saveData(key: String, value: Any) {
+        let newkey = key.lowercased()
         var d = loadData()
-        d[key] = value
+        d[newkey] = value
         savedData = d
         let _ = saveDataFile()
     }
     public class func removeSavedData(key: String) {
+        let newkey = key.lowercased()
         var d = loadData()
-        d.removeValue(forKey: key)
+        d.removeValue(forKey: newkey)
         savedData = d
         let _ = saveDataFile()
     }
