5892b9f7c861662b12ab0ab8a86c0ae440df58e0
Ray Deck <ray@raydeck.com>
2020-06-24 09:01:10 -0500

    Adding support for subscribing to events

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 import { Platform, NativeModules, NativeEventEmitter } from "react-native";
-//#region Code for object RNSRegistry
 const { RNSRegistry, RNKRegistryModule } = NativeModules;
+//#region event management
+let emitter;
+let subscriptions = {};
+if (Platform.OS === "ios") emitter = NativeEventEmitter(RNSRegistry);
+let listener = null;
+const updateSubscriptions = () => {
+  if (!emitter) return;
+  if (!Object.keys(subscriptions).length && listener) {
+    listener.remove();
+    listener = null;
+  } else if (!listener) {
+    listener = emitter.addEventListener(
+      "RNSRegistry",
+      ({ type, key, data }) => {
+        const compoundKey = [type, key].join("_");
+        if (subscriptions[compoundKey]) subscriptions[compoundKey](data);
+      }
+    );
+  }
+  if (emitter)
+    emitter.addEventListener("RNSRegistry", ({ type, key, data }) => {
+      const compoundKey = [type, key].join("_");
+      if (subscriptions[compoundKey]) subscriptions[compoundKey](data);
+    });
+};
+//#endregion
+//#region methods
 const setData = async (key, data) => {
   if (Platform.OS === "android")
     return await RNKRegistryModule.setData(key, data);
@@ -19,13 +45,22 @@ const getData = async (key) => {
   if (Platform.OS === "android") return await RNKRegistryModule.getData(key);
   return await NativeRNSRegistry.getData(key);
 };
-const addEvent = async (type, key = "RNAdvanced") => {
+const addEvent = async (type, key = "RNAdvanced", callback) => {
   if (Platform.OS === "android")
     return await RNKRegistryModule.addEvent(type, key);
-  return await NativeRNSRegistry.addEvent(type, key);
+  const compoundKey = [type, key].join("_");
+  if (subcriptions[compoundKey]) subscriptions[compoundKey].remove();
+  await NativeRNSRegistry.addEvent(type, key);
+  subscriptions[compoundKey] = callback;
+  updateSubscriptions();
 };
-const removeEvent = async (key, label = "RNAdvanced") => {
-  if (Platform.OS === "android") RNKRegistryModule.removeEvent(key, label);
+const removeEvent = async (type, key = "RNAdvanced") => {
+  if (Platform.OS === "android")
+    RNKRegistryModule.removeEvent(key, label, callback);
+  const compoundKey = [type, key].join("_");
+  if (subscriptions(compoundKey)) subscriptions[compoundKey].remove();
+  delete subscriptions[compoundKey];
+  updateSubscriptions();
   return await NativeRNSRegistry.removeEvent(key);
 };
 //#endregion
